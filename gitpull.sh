9786141658f939bbe69d218a5135a2ab26af76d4
MACKENZA <a.mackenzie@lancaster.ac.uk>
2014-11-19 11:20:12 +0000

    more snippets -- labfig

--- gitpull.sh
+++ gitpull.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 echo fetching dotfile
 cd ~/dotfile/
+git status -s
 git pull
+
 cd ~/ref_bibs/
 echo fetching ref_bibs
+git status -s
 git pull
+
 cd ~/document-archive/
 echo fetching document-archive
+git status -s
 git pull
+
 cd ~/notes/
 echo fetching notes
+git status -s
 git pull
