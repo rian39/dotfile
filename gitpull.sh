bf1a046321379199520bca114b21ba9b4cb42c4c
rian39 <a.mackenzie@lancaster.ac.uk>
2014-12-03 20:52:53 +0000

    improving format on gitpull etc

--- gitpull.sh
+++ gitpull.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 echo fetching dotfile
+echo ----------------------------
 cd ~/dotfile/
 git status -s
 git pull
 
 cd ~/ref_bibs/
+echo ----------------------------
 echo fetching ref_bibs
+echo ----------------------------
 git status -s
 git pull
 
 cd ~/document-archive/
+echo ----------------------------
 echo fetching document-archive
+echo ----------------------------
 git status -s
 git pull
 
 cd ~/notes/
+echo ----------------------------
 echo fetching notes
+echo ----------------------------
 git status -s
 git pull
--- gitpush.sh
+++ gitpush.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
+git config credential.helper 'cache --timeout=30'
 echo pushing dotfile
 cd ~/dotfile/
 git status -s
 git push
 
-cd ~/ref_bibs/
-echo pushing ref_bibs
-git status -s
-git push
 
 cd ~/document-archive/
 echo pushing document-archive
@@ -16,5 +13,12 @@ git push
 
 cd ~/notes/
 echo pushing notes
+git add ~/notes/*.md
+git commit -a -m 'adding new notes'
+git status -s
+git push
+
+cd ~/ref_bibs/
+echo pushing ref_bibs
 git status -s
 git push
